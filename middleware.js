ee665795df3450cd66283a1940a68ac2746b3c32
mahabube <info.freelancermahabub@gmail.com>
2023-09-13 19:13:43 +0600

    added middleware some info

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
-export {default} from "next-auth/middleware";
+import {withAuth} from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+import {NextResponse} from "next/server";
+
 export const config = {
   matcher: [
     "/dashboard/user/:path*",
@@ -7,3 +10,31 @@ export const config = {
     "/api/admin/:path*",
   ],
 };
+
+// role based authorization
+export default withAuth(
+  async function middleware(req) {
+    const url = req.nextUrl.pathname;
+    const userRole = req?.nextauth?.token?.user?.role;
+
+    // cors
+    if (url?.includes("/api")) {
+      NextResponse.next().headers.append("Access-Control-Allow-Origin", "*");
+    }
+    if (url.includes("/user") && userRole !== "user") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    if (url?.includes("/admin") && userRole !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({token}) => {
+        if (!token) {
+          return false;
+        }
+      },
+    },
+  }
+);
